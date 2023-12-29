1248adb341ba097e3a15f02ea227d643f0c2a73a
avee <avee@avees-MacBook-Air.local>
2023-12-30 00:46:43 +0600

    added soldier and bananas

---
+++ Soldier_and_Bananas.py
@@ -0,0 +1,10 @@
+k, n, w = map(int, input().split())
+total_cost = 0
+
+for i in range(w):
+    total_cost = total_cost + ((i+1) * k)
+
+if total_cost > n:
+    print(total_cost-n) 
+else:
+    print(0)
